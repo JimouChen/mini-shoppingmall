800d090950b2625ca3a5dc5c2ce8e4aad5e70b78
JimouChen <jmc1625645320@gmail.com>
2021-02-21 23:15:29 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,22 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // 轮播图数组
+    swiperList: [],
+    // 导航 数组
+    catesList:[],
+    // 楼层数据
+    floorList:[]
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  // 页面开始加载 就会触发
+  onLoad: function (options) {
+    // 1 发送异步请求获取轮播图数据  优化的手段可以通过es6的 promise来解决这个问题
+    // wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //       console.log(result)
+    //   }
+    // });
+
   }
 })
